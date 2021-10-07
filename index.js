12e79b18122e2772386af5bfbbb96e765c9cdc3d
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2021-10-07 00:28:35 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis","Garfield"];
+const newcat = "Ralph"
+function destructivelyAppendCat(newcat){
+    cats.push(newcat);
+}
+function destructivelyPrependCat(newcat){
+    cats.unshift(newcat);
+}
+function destructivelyRemoveLastCat(newcat){
+    cats.pop(3);
+}
+function destructivelyRemoveFirstCat(newcat){
+    cats.shift(0);
+}
+function appendCat(){
+    const copyCats = [...cats,"Broom"];
+    return copyCats;
+}
+function prependCat(){
+    const copyCats = ["Arnold",...cats];
+    return copyCats;
+}
+function removeLastCat(){
+    const copyCats = cats.slice(0,-1);
+    return copyCats;
+}
+function removeFirstCat(){
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
